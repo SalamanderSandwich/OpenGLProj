d33628074f52c78bfda5aae3796f8269582203e8
SalamanderSandwich <SalamanderSandwich@cock.li>
2017-12-27 00:12:45 -0600

    a

--- OpenGLProj/shader.fs
+++ OpenGLProj/shader.fs
@@ -7,6 +7,7 @@ uniform sampler2D texture1;
 uniform sampler2D texture2;
 void main()
 {
-	FragColor = mix(texture(texture1, texCord), texture(texture2, texCord), 1);
+	float mix_value = texture1.a * 0.2;
+	FragColor = mix(texture(texture1, texCord), texture(texture2, texCord), mix_value);
 //FragColor = vec4(outColor,1.0f);
 };
